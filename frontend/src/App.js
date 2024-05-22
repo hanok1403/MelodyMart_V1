9641ee78d45db4cb26b75e1307a5217d7777afba
G-Chandrakanth-16 <chandugotfan@gmail.com>
2024-05-23 00:52:44 +0530

    Styles demo added to nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,54 +1,9 @@
 import React from "react";
-import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import AddProduct from "./components/AddProduct";
-import Admin from "./components/Admin";
-import Cart from "./components/Cart";
-import DashBoard from "./components/DashBoard";
-import Home from "./components/Home";
-import Login from "./components/Login";
-import Orders from "./components/Orders";
-import Register from "./components/Register";
+import NavBar from "./components/NavBar";
 function App() {
   return (
     <div>
-      <Router>
-        <ul>
-          <li>
-            <Link to="/">To Home</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">To Profile</Link>
-          </li>
-          <li>
-            <Link to="/login"> Login </Link>
-          </li>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-          <li>
-            <Link to="/cart">Cart</Link>
-          </li>
-          <li>
-            <Link to="/orders">Orders</Link>
-          </li>
-          <li>
-            <Link to="/admin">Admin</Link>
-          </li>
-          <li>
-            <Link to="/addProduct">AddProduct</Link>
-          </li>
-        </ul>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<DashBoard />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/addProduct" element={<AddProduct />} />
-        </Routes>
-      </Router>
+      <NavBar />
     </div>
   );
 }
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "../styles/NavStyle.css";
+import AddProduct from "./AddProduct";
+import Admin from "./Admin";
+import Cart from "./Cart";
+import DashBoard from "./DashBoard";
+import Home from "./Home";
+import Login from "./Login";
+import Orders from "./Orders";
+import Register from "./Register";
+const NavBar = () => {
+  return (
+    <div>
+      <Router>
+      <div className="navbar">
+        <ul>
+          <li>
+            <Link to="/">To Home</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">To Profile</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/cart">Cart</Link>
+          </li>
+          <li>
+            <Link to="/orders">Orders</Link>
+          </li>
+          <li>
+            <Link to="/admin">Admin</Link>
+          </li>
+          <li>
+            <Link to="/addProduct">AddProduct</Link>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/addProduct" element={<AddProduct />} />
+      </Routes>
+    </Router>
+    </div>
+  )
+}
+
+export default NavBar;
