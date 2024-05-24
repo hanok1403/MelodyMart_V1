35fb2cc2103320e1df1cc62863a767a585ede8bf
G-Chandrakanth-16 <chandugotfan@gmail.com>
2024-05-24 14:33:04 +0530

    pulling to change the code

---
+++ frontend/src/components/userComponents/NavBar.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "../styles/NavStyle.css";
+import Cart from "./Cart";
+import Customer from './Customer';
+import Home from "./Home";
+import Orders from "./Orders";
+import Profile from "./Profile";
+import Register from "./Register";
+const NavBar = () => {
+  return (
+    <div>
+      <Router>
+      <div className="navbar">
+        <ul>
+          <li>
+            <Link to="/">To Home</Link>
+          </li>
+          <li>
+            <Link to="/profile">To Profile</Link>
+          </li>
+          <li>
+            <Link to="/customer">customer</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/cart">Cart</Link>
+          </li>
+          <li>
+            <Link to="/orders">Orders</Link>
+          </li>
+          <li>
+            <Link to="/admin/*">Admin</Link>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/customer" element={<Customer />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/admin/*" element={<Admin />} />
+      </Routes>
+    </Router>
+    </div>
+  )
+}
+
+export default NavBar;
