464471da12a723ea34b20c5d6aad3bfc2e8bc2e4
hanok1403 <hanokerugurala99@gmail.com>
2024-05-24 14:32:08 +0530

    small changes

--- frontend/src/components/admincomponents/Items.jsx
+++ frontend/src/components/admincomponents/Items.jsx
@@ -14,11 +14,12 @@ function Items(props) {
     };
   return (
     <div className="card h-100" key={props.key}>
-      <img src={props.product.imageUrl} alt="instrument" className="card-img-top" />
+      <img src={props.product.imageUrl} alt={props.product.productName} className="card-img-top" height="100px" width="100px"/>
       <div className="card-body d-flex flex-column">
         <h5 className="card-title">{props.product.productName}</h5>
         <p className="card-text">{props.product.description}</p>
         <h6 className="card-price">${props.product.price}</h6>
+        <p className="card-text">Remaining: {props.product.quantity}</p>
         <div className="mt-auto">
           <button onClick={()=>handleEdit(props.product.productId)} className="btn btn-primary mx-1">Edit</button>
           <button onClick={()=>handleRemove(props.product.productId)} className="btn btn-danger mx-1">Remove</button>
