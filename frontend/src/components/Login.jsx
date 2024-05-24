293d2032c24937ed8645d24f0661a0fae88017cb
AkhilReddyModugu <akhilreddymodugu13@gmail.com>
2024-05-24 16:10:07 +0530

    Create/Modify Login Component

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,40 +1,26 @@
-import React  from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom'; 
 import "../styles/login.css";
 import InputV from './InputV';
+
 const Login = (props) => {
-// const [userType,setUserType]=useState('[]');
-
-  // useEffect(() => {
-  //   if (props.type) {
-  //     setUserType(props.type);
-  //   }
-  // }, []);
-
-  // console.log(userType);
-  // const navigate = useNavigate();
-
-  // const handleClick=(event)=>{
-  //   event.preventDefault();
-  //   //handle login authentication here
-  //   if(userType==='admin')
-  //   navigate("/admin-dashboard");
-  //   else if(userType==='customer')
-  //   navigate("/");
-  // }
-
   return (
     <div className="login-wrapper">
-    <div className="login-container">
-      {/* <form action="post" onSubmit={handleClick}  className="login-form"> */}
-      <form action="post" className="login-form">
-        <div className="input-container">
-          <InputV type="text" name="email" id="email" value={"Enter your email"}/>
-          <InputV type="password" name="password" id="password" value={"Enter your password"}/>
-          <button  type="submit">Login</button>
+      <div className="login-container">
+        <h1>Login</h1>
+        <form action="post" className="login-form">
+          <div className="input-container">
+            <InputV type="text" name="email" id="email" value={"Enter your email"} ph="Email"/>
+            <InputV type="password" name="password" id="password" value={"Enter your password"} ph="Password" />
+            <button type="submit" className="login-button">Login</button>
+          </div>
+        </form>
+        <div className="signup-link">
+          <p>Don't have an account?</p> <Link to="/register">Sign up</Link>
         </div>
-      </form>
       </div>
     </div>
   )
 }
+
 export default Login;
